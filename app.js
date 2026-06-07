8e94e27b9fa91683d5392f96217050dd9bbec9a6
Rona <taguiamronalyn123@gmail.com>
2026-06-07 12:33:37 +0800

    add cors

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const connectToDb = require("./config/db");
 const router = require("./routes/routes");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 connectToDb();
 
